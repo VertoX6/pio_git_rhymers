570596b89b60a07b0c0b0649a5173861cf43f4ab
Jan Andrzejewski <255555@edu.p.lodz.pl>
2026-03-25 21:43:07 +0100

    Dodanie stałych, final oraz Override

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,11 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+	public static final int CAPACITY = 12;
+	 public static final int EMPTY = -1;
+	 public static final int VALUE = 4;
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
-        int testValue = 4;
+        int testValue = VALUE;
         rhymer.countIn(testValue);
 
         int result = rhymer.peekaboo();
@@ -30,7 +32,7 @@ public class RhymersJUnitTest {
     @Test
     public void testIsFull() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
-        final int STACK_CAPACITY = 12;
+        final int STACK_CAPACITY = CAPACITY;
         for (int i = 0; i < STACK_CAPACITY; i++) {
             boolean result = rhymer.isFull();
             Assert.assertEquals(false, result);
@@ -44,12 +46,12 @@ public class RhymersJUnitTest {
     @Test
     public void testPeekaboo() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
-        final int EMPTY_STACK_VALUE = -1;
+        final int EMPTY_STACK_VALUE = EMPTY;
 
         int result = rhymer.peekaboo();
         Assert.assertEquals(EMPTY_STACK_VALUE, result);
 
-        int testValue = 4;
+        int testValue = VALUE;
         rhymer.countIn(testValue);
 
         result = rhymer.peekaboo();
@@ -61,12 +63,12 @@ public class RhymersJUnitTest {
     @Test
     public void testCountOut() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
-        final int EMPTY_STACK_VALUE = -1;
+        final int EMPTY_STACK_VALUE = EMPTY;
 
         int result = rhymer.countOut();
         Assert.assertEquals(EMPTY_STACK_VALUE, result);
 
-        int testValue = 4;
+        int testValue = VALUE;
         rhymer.countIn(testValue);
 
         result = rhymer.countOut();
